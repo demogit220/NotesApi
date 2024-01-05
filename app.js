40acebe3d9731c13639ca4e8f3f897ee307e1034
demogit220 <gauravkumar_20329@aitpune.edu.in>
2024-01-05 23:00:52 +0530

    rate-limiter

--- app.js
+++ app.js
@@ -2,12 +2,18 @@ const express = require("express");
 const dotenv = require("dotenv");
 const userRoutes = require("./server/Routers/userRoutes");
 const cookieParser = require('cookie-parser');
+const rateLimit = require('express-rate-limit');
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api", userRoutes);
+const apiLimiter = rateLimit({
+  windowMs: 60*1000,
+  max: 5
+})
+
+app.use("/api", apiLimiter, userRoutes);
 
 
 app.get("*", (req, res) => {
