ae01ff75c79996cbeb79369fa8bc8f991bc8422f
demogit220 <gauravkumar_20329@aitpune.edu.in>
2024-01-05 23:18:09 +0530

    implement compression and other security features

--- app.js
+++ app.js
@@ -1,10 +1,36 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const userRoutes = require("./server/Routers/userRoutes");
 const cookieParser = require('cookie-parser');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
+const compression = require('compression');
+const cors = require('cors');
+
+const userRoutes = require("./server/Routers/userRoutes");
 
 const app = express();
+
+app.use(cors());
+app.options('*', cors());
+
+app.use(
+  helmet({
+    crossOriginEmbedderPolicy: false,
+    crossOriginResourcePolicy: {
+      allowOrigins: ['*'],
+    },
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ['*'],
+        scriptSrc: ["* data: 'unsafe-eval' 'unsafe-inline' blob:"],
+      },
+    },
+  })
+);
+
 app.use(express.json());
 app.use(cookieParser());
 
@@ -12,6 +38,11 @@ const apiLimiter = rateLimit({
   windowMs: 60*1000,
   max: 5
 })
+
+app.use(mongoSanitize());
+app.use(xss());
+app.use(hpp())
+app.use(compression());
 
 app.use("/api", apiLimiter, userRoutes);
 
